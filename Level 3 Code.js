34f45dd26308658f3ecffcb532d19da0cf1069a2
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-07-23 11:29:38 -0600

    Create Level 3 Code.js

---
+++ Level 3 Code.js
@@ -0,0 +1,18 @@
+function password() {
+    var firstNegConst = -6868010.582333334;
+    console.log("You've got this!", 7611281388.521244, 0x1b7);
+    var aBunchOfPowers = Math.pow(0x1551c, 0x2) + Math.pow(0x1b7, 0x2) - Math.pow(0.234, 0x2) + Math.pow(0xcd7, 0x2) - Math.pow(0x2a, 0x2);
+    var sqrt = Math.sqrt(1238405844954.0962);
+    var logs = Math.log(0x1551c) + Math.log(0x1b7) - Math.log(0.234) + Math.log(0xcd7) - Math.log(0x2a);
+    var trig = Math.sin(0x1551c) * Math.cos(0x1b7) + Math.tan(0.234) - Math.atan(0xcd7) + Math.acos(0x2a);
+    var floors = Math.floor(198.91571753986332) * Math.ceil(769.158) + Math.round(0x2a);
+    console.log(0x1551c, 0x1b7, 0.234, 0xcd7, 0x2a, 31521527.24457143, 3679.172, firstNegConst, 5391252.297428571, 7611281388.521244, aBunchOfPowers, sqrt, logs, trig, floors);
+    var secondNegConst = -140845.730256;
+    var numberWithRandom = (Math.floor(Math.random() * 0x1551c) + Math.ceil(Math.random() * 0x1b7)) * (Math.round(Math.random() * 0.234) - Math.random() * 0xcd7);
+    console.log("Red Herring");
+    console.log("That kilIs people!", "That krills people!", "not these either.", "That kills people.");
+    console.log("Pas", 'swo', "rd:", "Carl,", "That kills people!");
+    console.log("This isn't the password.");
+    console.log(0x1551c, 0x1b7, 0.234, 0xcd7, 3679.172, "Fudge14", numberWithRandom, secondNegConst, 5956925.961908181, aBunchOfPowers, sqrt);
+  }
+  password();
